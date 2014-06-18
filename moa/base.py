27c28d413d7fbca107181c4b4b82273afcb2eb03
Matthew Einhorn <moiein2000@gmail.com>
2014-06-18 17:46:28 -0400

    Clean the base moa class.

--- moa/base.py
+++ moa/base.py
@@ -3,8 +3,10 @@
 '''
 
 
+from weakref import ref
 from kivy.event import EventDispatcher
-from kivy.properties import StringProperty, OptionProperty
+from kivy.properties import StringProperty, OptionProperty, ObjectProperty
+import logging
 
 
 class MoaException(Exception):
@@ -13,42 +15,43 @@ class MoaException(Exception):
 
 class MoaBase(EventDispatcher):
 
-    _widgets = {}
-    _name = ''
+    named_moas = {}
+    ''' A weakref.ref to the named moa instances.
 
-    @staticmethod
-    def get_named_instance(name):
-        return MoaBase._widgets[name]
+    Read only.
+    '''
+    _last_name = ''
 
     def __init__(self, **kwargs):
-        self.size_hint = None, None
-        self.size = 0, 0
         super(MoaBase, self).__init__(**kwargs)
 
-        self.bind(name=self._verfiy_name)
+        def verfiy_name(instance, value):
+            named_moas = MoaBase.named_moas
+            old_name = self._last_name
+            if value == old_name:
+                return
 
-    def _verfiy_name(self, instance, value):
-        widgets = MoaBase._widgets
-        old_name = self._name
-        if value == old_name:
-            return
+            if old_name:
+                del named_moas[old_name]
 
-        if value and value in widgets:
-            raise ValueError('Moa instance with name {} already exists, {}'.
-                             format(value, widgets[value]))
-        if old_name:
-            del widgets[old_name]
-        if value:
-            widgets[value] = self
-        self._name = value
+            if value:
+                if value in named_moas and named_moas[value]() is not None:
+                    raise ValueError('Moa instance with name {} already '
+                        'exists: {}'.format(value, named_moas[value]()))
+                else:
+                    named_moas[value] = ref(self)
+            self._last_name = value
+
+        self.bind(name=verfiy_name)
+        verfiy_name(self, self.name)
 
     name = StringProperty('')
     ''' Unique name across all Moa objects
     '''
-    log_level = OptionProperty('debug', options=['debug', 'info', 'critical',
-                                                 'quiet'])
-    ''' How much to log, in addition to whatever the class saves.
-    '''
+
+    logger = ObjectProperty(logging.getLogger('moa'),
+                            baseclass=logging.Logger)
+
     source = StringProperty('')
     ''' E.g. a filename to load that interpreted by the subclass.
     '''
