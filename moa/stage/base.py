257c6307b1aab07ef677746404595aa2b56d42f7
Matthew Einhorn <moiein2000@gmail.com>
2014-06-20 13:54:31 -0400

    Remove stop/start dispatching, rename increment_loop to step_stage and decouple the different methods.

--- moa/stage/base.py
+++ moa/stage/base.py
@@ -26,23 +26,12 @@ class StageRender(object):
 
 class MoaStage(MoaBase, Widget):
 
-    __events__ = ('on_start', 'on_stop', )
-    ''' stop is dispatched when the children and itself needs to stop.
-    '''
-
     _pause_list = []
     ''' The list of children we ourselves have paused and we need to unpause
     after we ourself become unpaused. Children that were already paused when
     we paused do not have to be unpaused.
     '''
-    _loop_done = False
-    ''' If the stage loop itself is done, this is True, need it in case
-    children not done, so we know that we are ready to finish.
-    '''
-    _loop_end = False
-    ''' If the stage loop itself is done, this is True, need it in case
-    children not done, so we know that we are ready to finish.
-    '''
+    _loop_finishing = False
     _schedule_queue = None
     _schedule_increment = None
 
@@ -54,7 +43,7 @@ class MoaStage(MoaBase, Widget):
         self._schedule_queue = CallbackDeque(
                                     Clock.create_trigger(self._service_queue))
         self._schedule_increment = \
-        lambda dt: self.parent.increment_loop(source=self)
+        lambda dt: self.parent.step_stage(source=self)
 
     def get_state(self, state=None):
         if state is None:
@@ -140,33 +129,6 @@ class MoaStage(MoaBase, Widget):
                             'remove {}'.format(widget))
         obj.stage_render.remove_render(self, widget, **kwargs)
 
-    def on_start(self, **kwargs):
-        '''asynchrony a stop signal might arrive before start. Of course
-        don't be an a-hole and randomly start stages when it's supposed to
-        finish, just that it's ok
-        must return if stage was started, false otherwise. True means it was
-        eaten - stage started. So true stops the dispatch (unless parallel).
-        this must be called through super.
-
-        When paused, we still start the next stage. The stage is responsible for
-        pausing, once started.
-        '''
-        if self.disabled:
-            self.logger.debug('Skipped starting disabled stage')
-            return False
-        if self.started and not self.finished:
-            self.logger.debug('Already started stage - restarting')
-        self.logger.trace('Starting stage')
-
-        self.clear()
-        # the stage needs to end after max_duration if provided
-        if not self.paused and self.max_duration > 0.:
-            Clock.schedule_once(self._do_stage_timeout, self.max_duration)
-
-        # start the children and everything going
-        self.increment_loop(source=self, start=True)
-        return True
-
     def on_paused(self, instance, value, recurse=True, **kwargs):
         if self.disabled or not self.started or self.finished:
             return
@@ -191,34 +153,29 @@ class MoaStage(MoaBase, Widget):
                 for child in self._pause_list:
                     child.paused = False
 
-    def on_stop(self, source=None, force=False, **kwargs):
-        '''When called, it must stop stage and finished must be True if it
-        returns False. Whether started or not.
-        If it returns True, dispatching stops. To allow dispatching to reach
-        everywhere, return False. It is the responsibility of the one returning
-        True, to dispatch stop with original source  at source when ready to
-        stop.
+    def stop(self, stage=True, **kwargs):
+        '''Stops async recursively. Use self.finished to check when really
+        stopped.
+
+        When this is called with super, this instance loop_done is set to True.
         '''
         if not self.started or self.finished:
             return False
-        for c in self.stages[:]:
-            if c.dispatch('on_stop', source=source, force=force, **kwargs):
-                assert not force
-                return True
+
         # all children were stopped, so we can stop now
         Clock.unschedule(self._do_stage_timeout)
-        self.finished = True
-        if source is self:
-            parent = self.parent
-            if parent is not None:
-                Clock.schedule_once(self._schedule_increment)
+        if stage:
+            self.finishing = True
+        else:
+            self._loop_finishing = True
+        self.step_stage(source=self)
         return False
 
     def on_disabled(self, instance, value, **kwargs):
         ''' Dispatch.
         '''
         if value and self.started and not self.finished:
-            self.dispatch('on_stop', source=self, force=True)
+            self.stop()
 
     def clear(self, recurse=False, **kwargs):
         '''Clears started etc. stops running if running.
@@ -232,106 +189,139 @@ class MoaStage(MoaBase, Widget):
         self.count = 0
         self.elapsed_time = 0.
         self.start_time = 0.
-        self._loop_done = False
-        self._loop_end = False
+        self.loop_done = False
+        self.finishing = False
+        self._loop_finishing = False
         if recurse:
             for child in self.stages[:]:
                 child.clear(recurse)
         return True
 
-    def increment_loop(self, source=None, **kwargs):
+    def step_stage(self, source=None, **kwargs):
         ''' Only allowed to be called when after on_stop, or if class finished
-        lopp on its own.
+        lopp on its own in which case finishing is True.
 
         source None is the same as self.
-        '''
-        if self.finished:
-            self.logger.exception('Ignored increment on finished stage')
-            return False
 
-        start = kwargs.get('start', False)
-        children = self.stages
+        When source is self (None) and we need to step the loop, it may only
+        return false if didn't start b/c not finished or disabled. To prevent
+        from starting set it to disabled, and not by just returning here False.
+        Otherwise, we may never proceed to the next stage because the stop
+        condition is not met, but start returns False.
+        '''
+
+        children = self.stages[:]
         comp_list = self.completion_list
         comp_type = self.completion_type
         order = self.order
-
-        if start:
-            for child in children:
-                child.clear()
-            self._loop_end = True
+        logger = self.logger
         if source is None:
             source = self
+        start = (not self.started or self.finished) and source is self
+
+        if start:
+            if self.disabled:
+                logger.debug('Skipped starting disabled stage')
+                return False
+            logger.trace('Starting stage')
+            self.clear()
+            for child in children:
+                child.clear()
+
+            self.start_time = time.clock()
+            self.started = True
+            if not self.paused and self.max_duration > 0.:
+                Clock.schedule_once(self._do_stage_timeout, self.max_duration)
+        elif not self.started and source is not self:
+            logger.warning('Ignored step_stage for source {}, because'
+                           ' stage is not started'.format(source))
+            return False
+        elif self.finished:
+            logger.error('Ignored step_stage (source={}) on finished '
+                         'stage'.format(source))
+            return False
+
+        # decide if this loop iteration is done
         if source is self and not start:
-            self._loop_done = True
-        loop_done = self._loop_done
-        done = self._loop_end = (self._loop_end or
-            (comp_type == 'any' and
+            self.loop_done = True
+        loop_done = self.loop_done
+        done = self._loop_finishing = (self.finishing or self._loop_finishing
+            or (comp_type == 'any' and
             (not comp_list or (loop_done and self in comp_list) or
-             any([c.finished and not c.disabled for c in comp_list]))) or
-            (comp_type == 'all' and
+             any([c.finished and not c.disabled for c in comp_list])))
+            or (comp_type == 'all' and
             ((comp_list and all([(c.finished or c.disabled) and c is not self
             or c is self and loop_done for c in comp_list]))
             or (not comp_list and loop_done and
             all([c.finished for c in children])))))
 
+        # if we need to finish, stop all the children
         i = None
-        if not start:
-            for k in range(len(children)):
-                child = children[k]
-                if child.disabled:
-                    continue
-                if not child.finished:
-                    if done:
-                        child.dispatch('on_stop', source=child)
-                        return False
-                    elif child.started:
-                        return False
-                    elif i is None:
-                        i = k
+        if done:
+            for child in children:
+                if not child.disabled and child.started and not child.finished:
+                    child.stop()
+                    return False
+            if not loop_done:
+                self.stop(stage=False)
+                return False
+        elif not start:
+            # if parallel, all should be started so just wait until we done
+            if order == 'parallel':
+                return False
+            # if serial see if there's a next child to start, otherwise wait to
+            # finish
+            else:
+                # if we don't need to finish, find the first not started child
+                for k in range(len(children)):
+                    child = children[k]
+                    if not child.disabled and not child.finished:
+                        # if a child is already running we cannot proceed
+                        if child.started:
+                            return False
+                        elif i is None:
+                            i = k
+                            break
 
-        # if not done, all children are not started otherwise they're finished
-        if not done:
-            # i == None means all children finished
-            if i is None:
-                if loop_done:   # if we're finished: no more children = done
-                    done = True
-                else:           # otherwise, go and wait until we finish
+                # if we didn't find a child to start, then just wait
+                if i is not None:
+                    for child in children[i:]:
+                        if child.step_stage():
+                            return False
+                logger.warning('Could not start a child; this code should '
+                               'not have been reached')
+                # if no child was started, we may now need to finish
+                if not loop_done:
                     return False
 
-        # if done, then all children are finished
-        if done:
-            # now we need to increment the loop
-            if (self.repeat == -1 or
-                self.count + (0 if start else 1) < self.repeat):
+        # if we reached here then either start or (loop_done and all children
+        # are done)
+        if not self.finishing and (start or self.repeat == -1 or
+            self.count + (0 if start else 1) < self.repeat):
+
+            if not start:
                 for child in children:
                     if not child.disabled:
                         child.clear()
+                self.loop_done = self._loop_finishing = False
+                t = time.clock()
+                self.elapsed_time += t - self.start_time
+                self.start_time = t
+                self.count += 1
 
-                self._loop_done = self._loop_end = False
-                if not start:
-                    self.elapsed_time += time.clock() - self.start_time
-                    self.start_time = time.clock()
-                    self.count += 1
-                else:
-                    self.start_time = time.clock()
-                    self.started = True
+            if order == 'serial':
+                for child in children:
+                    if child.step_stage():
+                        break
+            else:
+                for child in children:
+                    child.step_stage()
+            return True
 
-                if order == 'serial':
-                    for child in children:
-                        if child.dispatch('on_start'):
-                            break
-                else:
-                    for child in children:
-                        child.dispatch('on_start')
-                return True
-            self.dispatch('on_stop', source=self)
-            return False
-
-        # here, i is the child that is not started yet.
-        assert order == 'serial'
-        for child in children[i:]:
-            if child.dispatch('on_start'):
-                break
+        self.finished = True
+        parent = self.parent
+        if parent is not None:
+            parent.step_stage(source=self)
         return False
 
     def _service_queue(self, dt):
@@ -346,7 +336,7 @@ class MoaStage(MoaBase, Widget):
     def _do_stage_timeout(self, *l):
         if (self.max_duration > 0. and time.clock() - self.start_time +
             self.elapsed_time >= self.max_duration):
-            self.dispatch('on_stop', source=self)
+            self.stop()
 
     def __repr__(self):
         if self.finished:
@@ -433,4 +423,13 @@ class MoaStage(MoaBase, Widget):
 
     paused = BooleanProperty(False)
     ''' If we are paused.
+    '''
+
+    loop_done = BooleanProperty(False)
+    ''' If the stage loop itself is done, this is True, need it in case
+    children not done, so we know that we are ready to finish.
+    '''
+
+    finishing = BooleanProperty(False)
+    ''' If the stage is trying to finish.
     '''
