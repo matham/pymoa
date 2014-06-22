23f95f0319c929006c89efdf0d113370a1a003b4
Matthew Einhorn <moiein2000@gmail.com>
2014-06-21 22:20:29 -0400

    Update factory registers with stages.

--- moa/factory_registers.py
+++ moa/factory_registers.py
@@ -1,9 +1,11 @@
 from kivy.factory import Factory
 
 r = Factory.register
-r('MoaStage', module='moa.stage.base')
-r('StageRender', module='moa.stage.base')
+r('MoaStage', module='moa.stage')
 r('Delay', module='moa.stage.delay')
+r('GateStage', module='moa.stage.gate')
+
+r('StageRender', module='moa.stage.base')
 r('TreeRender', module='moa.render.treerender')
 r('TreeRenderExt', module='moa.render.treerender')
 r('StageTreeNode', module='moa.render.treerender')
