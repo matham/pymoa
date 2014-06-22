dc840d318b2cc35930a3ddbfb6959bd2f9e4c3b7
Matthew Einhorn <moiein2000@gmail.com>
2014-06-22 13:54:19 -0400

    Update the example to reflect the changes.

--- examples/example.py
+++ examples/example.py
@@ -1,9 +1,21 @@
 
 from kivy.config import Config
 Config.set('kivy', 'exit_on_escape', 0)
+from kivy.lang import Builder
 from moa.app import MoaApp
-from moa.stage.base import MoaStage
+from moa.stage import MoaStage
 from moa.render.treerender import TreeRender
+
+kv = '''
+BoxLayout:
+    button: button
+    Splitter:
+        sizable_from: 'left'
+        GridLayout:
+            cols: 1
+            ToggleButton:
+                id: button
+'''
 
 
 class RootStage(MoaStage):
@@ -16,13 +28,17 @@ class PlaygroundApp(MoaApp):
     root_renderer = None
 
     def build(self):
+        root = Builder.load_string(kv)
+
         renderer = self.root_renderer = TreeRender()
-        root = self.root_stage = RootStage(stage_render=renderer)
-        renderer.set_root_stage(root)
-        return renderer.get_root_widget()
+        root_stage = self.root_stage = RootStage()
+        renderer.build_tree(root_stage)
+
+        root.add_widget(renderer.root_widget, len(root.children))
+        return root
 
     def on_start(self):
-        self.root_stage.dispatch('on_start')
+        self.root_stage.step_stage()
 
 if __name__ == '__main__':
     app = PlaygroundApp()
