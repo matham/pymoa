5d2879ad625d4e64b3382199e6545ad2cf10207b
Matthew Einhorn <moiein2000@gmail.com>
2014-07-20 12:13:47 -0400

    Update factory registres.

--- moa/factory_registers.py
+++ moa/factory_registers.py
@@ -1,9 +1,25 @@
 from kivy.factory import Factory
 
 r = Factory.register
-r('MoaStage', module='moa.stage')
-r('Delay', module='moa.stage.delay')
-r('GateStage', module='moa.stage.gate')
 
 r('StageTreeNode', module='moa.render.treerender')
 r('StageSimpleDisplay', module='moa.render.stage_simple')
+
+
+# --------------------- devices -----------------------------
+r('Device', module='moa.device')
+r('DigitalChannel', module='moa.device.digital')
+r('DigitalPort', module='moa.device.digital')
+r('ButtonChannel', module='moa.device.digital')
+r('ButtonPort', module='moa.device.digital')
+
+r('AnalogChannel', module='moa.device.analog')
+r('AnalogPort', module='moa.device.analog')
+r('NumericPropertyChannel', module='moa.device.analog')
+r('NumericPropertyPort', module='moa.device.analog')
+
+
+# ---------------------- stages --------------------------------
+r('MoaStage', module='moa.stage')
+r('Delay', module='moa.stage.delay')
+r('GateStage', module='moa.stage.gate')
