3c1203d5f4665873e34de9600c6cf18cbd7f7611
Matthew Einhorn <moiein2000@gmail.com>
2014-08-05 16:05:53 -0400

    Add 2d list to ConfigProperty.

--- moa/tools.py
+++ moa/tools.py
@@ -3,9 +3,8 @@ __all__ = ('to_bool', 'ConfigPropertyList')
 
 from kivy.properties import ConfigParserProperty
 from re import compile, split
-from functools import partial
 
-to_list_pat = compile(', *')
+to_list_pat = compile('(?:, *)?\\n?')
 
 
 def to_bool(val):
@@ -17,18 +16,33 @@ def to_bool(val):
     return not not val
 
 
-def ConfigPropertyList(val, section, key, config, val_type, **kwargs):
+def ConfigPropertyList(val, section, key, config, val_type, inner_list=False,
+                       **kwargs):
+    ''' Accepts either a list of a string. Nothing else.
+    '''
     def to_list(val):
         if isinstance(val, list):
-            vals = val
+            vals = list(val)
         else:
             vals = split(to_list_pat, val.strip(' []()'))
         for i, v in enumerate(vals):
             vals[i] = val_type(v)
         return vals
 
+    def to_2d_list(val):
+        if isinstance(val, list):
+            vals = list(val)
+        else:
+            vals = [split(to_list_pat, line.strip(' []()'))
+                    for line in val.strip(' []()').splitlines()]
+        for i, line in enumerate(vals):
+            for j, v in enumerate(line):
+                vals[i][j] = val_type(v)
+        return vals
+
     if not isinstance(val, list):
-        val = [val]
-    return ConfigParserProperty(val, section, key, config, val_type=to_list,
+        val = [[val]] if inner_list else [val]
+    v_type = to_2d_list if inner_list else to_list
+    return ConfigParserProperty(val, section, key, config, val_type=v_type,
                                 **kwargs)
 
