6a43845a518ddfb8c2892aa6e0419d14eaf99fc4
Matthew Einhorn <moiein2000@gmail.com>
2014-10-10 14:50:28 -0400

    Exapnd ConfigProperty list and dict representation and remove the list processing from MoaConfig.

--- moa/config.py
+++ moa/config.py
@@ -5,10 +5,4 @@ class ConfigParser(KivyConfigParser):
     ''' Also handles 2d lists
     '''
 
-    def set(self, section, option, value):
-        if value and isinstance(value, (list, tuple)):
-            if isinstance(value[0], (list, tuple)):
-                value = '\n'.join([', '.join(map(str, v)) for v in value])
-            else:
-                value = ', '.join(map(str, value))
-        return super(ConfigParser, self).set(section, option, value)
+    pass
--- moa/tools.py
+++ moa/tools.py
@@ -1,10 +1,13 @@
 
-__all__ = ('to_bool', 'ConfigPropertyList')
+__all__ = ('to_bool', 'ConfigPropertyList', 'ConfigPropertyDict', 'StringList',
+           'String2DList', 'StringDict')
 
 from kivy.properties import ConfigParserProperty
 from re import compile, split
+from copy import deepcopy
 
 to_list_pat = compile('(?:, *)?\\n?')
+to_dict_pat = compile('(?:: *)?\\n?')
 
 
 def to_bool(val):
@@ -16,33 +19,78 @@ def to_bool(val):
     return not not val
 
 
+class StringList(list):
+    def __str__(self):
+        return ', '.join(map(str, self))
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class String2DList(list):
+    def __str__(self):
+        return '\n'.join([', '.join(map(str, item)) for item in self])
+
+    def __repr__(self):
+        return self.__str__()
+
+
 def ConfigPropertyList(val, section, key, config, val_type, inner_list=False,
                        **kwargs):
     ''' Accepts either a list of a string. Nothing else.
     '''
     def to_list(val):
         if isinstance(val, list):
-            vals = list(val)
+            vals = StringList(val)
         else:
-            vals = split(to_list_pat, val.strip(' []()'))
+            vals = StringList(split(to_list_pat, val.strip(' []()')))
         for i, v in enumerate(vals):
             vals[i] = val_type(v)
         return vals
 
     def to_2d_list(val):
         if isinstance(val, list):
-            vals = list(val)
+            vals = String2DList(deepcopy(val))
         else:
-            vals = [split(to_list_pat, line.strip(' []()'))
-                    for line in val.strip(' []()').splitlines()]
+            vals = String2DList([split(to_list_pat, line.strip(' []()'))
+                                 for line in val.strip(' []()').splitlines()])
         for i, line in enumerate(vals):
             for j, v in enumerate(line):
                 vals[i][j] = val_type(v)
         return vals
 
-    if not isinstance(val, list):
-        val = [[val]] if inner_list else [val]
     v_type = to_2d_list if inner_list else to_list
+    if not isinstance(val, list):
+        val = v_type([[val]]) if inner_list else StringList([val])
     return ConfigParserProperty(val, section, key, config, val_type=v_type,
                                 **kwargs)
 
+
+class StringDict(dict):
+
+    def __str__(self):
+        return '\n'.join(['{}: {}'.format(k, v) for k, v in
+                          sorted(self.items(), key=lambda x: x[0])])
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def ConfigPropertyDict(val, section, key, config, val_type, key_type,
+                       **kwargs):
+    ''' Accepts either a dict of a string. Nothing else.
+    '''
+    def to_dict(val):
+        if isinstance(val, dict):
+            vals = StringDict(val)
+        else:
+            vals = [split(to_dict_pat, line.strip(' '), maxsplit=1)
+                    for line in val.strip(' }{').splitlines()]
+            vals = StringDict({k: v for k, v in vals})
+
+        return StringDict({key_type(k): val_type(v) for k, v in vals.items()})
+
+    if not isinstance(val, dict):
+        val = to_dict(val)
+    return ConfigParserProperty(val, section, key, config, val_type=to_dict,
+                                **kwargs)
