ac379c249470e432e8f1a6a940d323ee199d0add
Matthew Einhorn <moiein2000@gmail.com>
2015-01-07 18:52:22 -0500

    Polish the compat module.

--- moa/compat.py
+++ moa/compat.py
@@ -6,10 +6,13 @@ __all__ = ('decode_dict', 'PY2', 'unicode_type', 'bytes_type')
 import sys
 
 PY2 = sys.version_info[0] == 2
+'''Whether the python version is 2.x (True), or 3.x (False).
+'''
 
 
 def unicode_type(val):
-    ''' Converts to `unicode` type.
+    ''' Converts `val` to `unicode` type. If the default encoding fails, it
+    tries with `utf8`.
     '''
     try:
         return (unicode if PY2 else str)(val)
@@ -18,7 +21,8 @@ def unicode_type(val):
 
 
 def bytes_type(val):
-    ''' Converts to `bytes`.
+    ''' Converts `val` to `bytes` type. If the default encoding fails, it
+    tries with `utf8`.
     '''
     try:
         return bytes(val)
@@ -26,8 +30,9 @@ def bytes_type(val):
         return val.encode('utf8')
 
 
-# from https://stackoverflow.com/questions/956867
 def _decode_list(data):
+    '''See :func:`decode_dict`.
+    '''
     rv = []
     for item in data:
         if isinstance(item, unicode):
@@ -41,7 +46,26 @@ def _decode_list(data):
 
 
 def decode_dict(data):
-    ''' Only python2 compatibale.
+    '''Method which takes a dict `data` and recursively converts it keys/values
+    that are unicode objects to bytes objects.
+
+    This is typically used with json. See
+    https://stackoverflow.com/questions/956867.
+
+    E.g.::
+
+        >>> import json
+        >>>d_dump = json.dumps({'a': 55, 'b': '33', 4: {1: 'a'}, 8: ['a', 'b']})
+        >>> d_dump
+        '{"a": 55, "8": ["a", "b"], "b": "33", "4": {"1": "a"}}'
+        >>> json.loads(d_dump)
+        {u'a': 55, u'8': [u'a', u'b'], u'b': u'33', u'4': {u'1': u'a'}}
+        >>> json.loads(d_dump, object_hook=decode_dict)
+        {'a': 55, '8': ['a', 'b'], 'b': '33', '4': {'1': 'a'}}
+
+    .. warning::
+        Function is only python2 compatible. It is not typically required
+        in py3.
     '''
     rv = {}
     for key, value in data.iteritems():
