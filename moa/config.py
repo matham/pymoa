963a78f47021a5e5b32dfc5ce490cdcf7aed8912
Matthew Einhorn <moiein2000@gmail.com>
2015-01-09 17:10:24 -0500

    Update config module.

--- moa/config.py
+++ moa/config.py
@@ -1,8 +1,13 @@
+'''Config module based on the kivy :kivy:class:`~kivy.config.ConfigParser`.
+'''
+
+__all__ = ('ConfigParser', )
+
 from kivy.config import ConfigParser as KivyConfigParser
 
 
 class ConfigParser(KivyConfigParser):
-    ''' Also handles 2d lists
+    '''Config parser class. Currently it's identical to
+    :kivy:class:`~kivy.config.ConfigParser`.
     '''
-
     pass
