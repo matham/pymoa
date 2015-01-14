3f8f3b8bbb0cd070d58865a45c5239f4bf72bc97
Matthew Einhorn <moiein2000@gmail.com>
2015-01-14 16:02:01 -0500

    Add logger classes.

--- moa/logger.py
+++ moa/logger.py
@@ -1,76 +1,215 @@
+'''The Moa logger module provides a logger class that is forwarded to kivy.
+It creates a base logger with name `moa` from which all moa logger are derived.
 
+Logs that pass the filter and are emitted by the moa logger, are forwarded to
+the kivy logger and emitted based on kivy's log level. Therefore, both the moa
+and kivy logger must be configured to the correct level for emission.
 
-__all__ = ('Logger', 'LOG_LEVELS')
+For example::
 
+    >>> from kivy.logger import Logger as KivyLogger
+    >>> from moa.logger import Logger
+    >>> import logging
 
+    >>> KivyLogger.setLevel(logging.INFO)
+    >>> Logger.setLevel(logging.DEBUG)
+    >>> Logger.debug('debug hello')
+    >>> Logger.info('info hello')
+    [INFO              ] [Moa         ] 2015-01-14 14:34:49,936 moa.<module>:8\
+ info hello
+    >>> KivyLogger.setLevel(logging.DEBUG)
+    >>> Logger.debug('debug hello')
+    [DEBUG             ] [Moa         ] 2015-01-14 14:34:49,937 moa.<module>:\
+10 debug hello
+'''
+
+__all__ = ('Logger', 'MoaObjectLogger')
+
+from kivy.logger import Logger as KivyLogger
+from kivy.logger import LOG_LEVELS
+
+from kivy.properties import (
+    ObjectProperty, ListProperty, OptionProperty, StringProperty)
 import logging
-import os
-from os.path import exists, join
-from time import strftime
 from functools import partial
-from moa.compat import PY2
-
-_filehandler = Logger = None
-logging.TRACE = 9
-LOG_LEVELS = {
-    'trace': logging.TRACE,
-    'debug': logging.DEBUG,
-    'info': logging.INFO,
-    'warning': logging.WARNING,
-    'error': logging.ERROR,
-    'critical': logging.CRITICAL}
 
 
-
-def set_logfile(filepath, filename):
-    ''' `%_` parts of `filename` will be replaced with a number (if exists)
-    creating a unique filename. Then, the filename will be passed to
-    `strftime`.
+def logger_config_update(section, key, value):
+    '''Callback called when the :attr:`~moa.config.Config` object changes the
+    `log_level` key value.
     '''
-    if not os.path.isdir(filepath):
-        os.makedirs(filepath)
-    pattern = join(filepath, strftime(filename.replace('%_', '@@NUMBER@@')))
-
-    n = 0
-    while True:
-        filename = pattern.replace('@@NUMBER@@', str(n))
-        if not exists(filename):
-            break
-        n += 1
-        if n > 10000:  # prevent maybe flooding ?
-            raise Exception('Too many logfile, remove some')
-
-    if _filehandler.filename == filename:
-            return
-    _filehandler.filename = filename
-    if _filehandler.fd is not None:
-        _filehandler.fd.close()
-    _filehandler.fd = open(filename, 'w')
-
-    Logger.info('Logger: Record log in %s' % filename)
+    if LOG_LEVELS.get(value) is None:
+        raise AttributeError('Loglevel {0!r} doesn\'t exists'.format(value))
+    Logger.setLevel(level=LOG_LEVELS.get(value))
 
 
-class FileHandler(logging.Handler):
-    filename = ''
-    fd = None
+class _MoaLoggerBase(logging.Logger):
+    '''Moa logger class which optimizes emissions by initially checking whether
+    the kivy logger is enabled for this level.
+    '''
+
+    def isEnabledFor(self, lvl):
+        if super(_MoaLoggerBase, self).isEnabledFor(lvl):
+            return KivyLogger.isEnabledFor(lvl)
+        return False
+
+
+class KivyHandler(logging.Handler):
+    '''The Moa handler class that forwards logs to kivy.
+    '''
 
     def emit(self, record):
-        fd = self.fd
-        if fd is None:
+        if KivyLogger.isEnabledFor(record.levelno):
+            KivyLogger.log(
+                record.levelno, 'Moa: {}'.format(self.format(record)))
+
+_orig_cls = logging.getLoggerClass()
+logging.setLoggerClass(_MoaLoggerBase)
+Logger = logging.getLogger('moa')
+'''The `moa` logger instance.
+'''
+logging.setLoggerClass(_orig_cls)
+Logger.trace = partial(Logger.log, logging.TRACE)
+_kivyhandler = KivyHandler()
+_kivyhandler.setFormatter(logging.Formatter(
+    '%(asctime)s %(name)s.%(funcName)s:%(lineno)d %(message)s'))
+Logger.addHandler(_kivyhandler)
+
+
+class MoaObjectLogger(object):
+    '''This is a base class, which when combined with a
+    :kivy:class:`~kivy.event.EventDispatcher` derived class will allow
+    automatic logging of the properties and events.
+
+    .. warning::
+        This calls must be combined with a
+        :kivy:class:`~kivy.event.EventDispatcher` type class, otherwise it will
+        raise exceptions.
+
+    For example::
+
+        >>> from moa.logger import MoaObjectLogger
+        >>> from kivy.uix.widget import Widget
+        >>> class LoggedWidget(MoaObjectLogger, Widget):
+        ...     pass
+
+        >>> wid = LoggedWidget(logged_attrs=['height', 'logged_attrs', \
+'logged_pat'])
+        >>> wid.height = 10
+        [DEBUG             ] [Moa         ] 2015-01-14 15:35:17,918 moa.logge\
+r.log_property_dispatch:138 height=10
+        >>> wid.logged_pat = '[widget wid] {}'
+        [DEBUG             ] [Moa         ] 2015-01-14 15:35:17,918 moa.logger\
+.log_property_dispatch:138 [widget wid] logged_pat=[widget wid] {}
+        >>> wid.width = 10
+        >>> # notice no output
+        >>> wid.logged_attrs = ['width']
+        >>> # notice no output here because the logged_* variables are changed\
+ before the log output
+        >>> wid.width = 50
+        [DEBUG             ] [Moa         ] 2015-01-14 15:35:17,918 moa.logger\
+.log_property_dispatch:138 [widget wid] width=50
+
+    '''
+
+    def __init__(self, **kwargs):
+        super(MoaObjectLogger, self).__init__(**kwargs)
+        if self.logger is not None:
+            self.fast_bind('logged_attrs', self._update_bound_loggers, 'bind')
+            self.fast_bind(
+                'log_attrs_type', self._update_bound_loggers, 'bind')
+            self._update_bound_loggers()
+
+    def _update_bound_loggers(self, action='bind', *largs):
+        fast_unbind = self.fast_unbind
+        for event in self.events():
+            fast_unbind(event, self.log_event_dispatch, event)
+        for prop in self.properties():
+            fast_unbind(prop, self.log_property_dispatch, prop)
+        if action == 'unbind':
             return
 
-        fd.write('[%-18s] ' % record.levelname)
-        try:
-            fd.write(record.msg)
-        except UnicodeEncodeError:
-            if PY2:
-                fd.write(record.msg.encode('utf8'))
-        fd.write('\n')
-        fd.flush()
+        fast_bind = self.fast_bind
+        events, props = self.events(), self.properties()
+        attrs = self.logged_attrs
+        select = self.log_attrs_type
+        if select == 'include':
+            events = [e for e in attrs if e in events]
+            props = [p for p in attrs if p in props]
+        else:
+            if attrs:
+                events = [e for e in events if e not in attrs]
+                props = [p for p in props if p not in attrs]
 
+        for event in events:
+            fast_bind(event, self.log_event_dispatch, event)
+        for prop in props:
+            fast_bind(prop, self.log_property_dispatch, prop)
 
-Logger = logging.getLogger('moa')
-Logger.trace = partial(Logger.log, logging.TRACE)
-_filehandler = FileHandler()
-Logger.addHandler(_filehandler)
-Logger.addHandler(logging.StreamHandler())
+    def log_property_dispatch(self, name, instance, value):
+        '''The callback that is executed when a property is changed.
+
+        :Parameters:
+
+            `name`: str
+                The name of the event.
+            `instance`: object
+                The object of which the event changed (this class).
+            `value`: object
+                The new value taken by property `name`.
+        '''
+        logger = self.logger
+        if logger is not None:
+            logger.debug(self.logged_pat.format('{}={}'.format(name, value)))
+
+    def log_event_dispatch(self, name, instance):
+        '''The callback that is executed when a event is triggered.
+
+        :Parameters:
+
+            `name`: str
+                The name of the event.
+            `instance`: object
+                The object of which the event changed (this class).
+        '''
+        logger = self.logger
+        if logger is not None:
+            logger.debug(self.logged_pat.format('{}'.format(name)))
+
+    log_attrs_type = OptionProperty('include', options=['exclude', 'include'])
+    '''Whether the :attr:`logged_attrs` list indicates the properties and to
+    include or exclude from being logged.
+
+    :attr:`log_attrs_type` is a :kivy:class:`~kivy.properties.OptionProperty`
+    and defaults to `'include'` (i.e. nothing is logged). Allowed value are
+    `'exclude'` and `'include'`. See :class:`MoaObjectLogger` for details.
+    '''
+
+    logged_attrs = ListProperty([])
+    '''A list of event and property names used to decide which events and
+    properties are logged.
+
+    If :attr:`log_attrs_type` is `include`, then only properties or events in
+    this list will be logged. If it's `exclude`, all properties and events,
+    except those in the list, will be logged.
+
+    :attr:`logged_attrs` is a :kivy:class:`~kivy.properties.ListProperty` and
+    defaults to `[]`. See :class:`MoaObjectLogger` for an example.
+    '''
+
+    logged_pat = StringProperty('{}')
+    '''The pattern used when logging to :attr:`logger`. The message will be
+    added with format on the string. Therefore, if it contains a `'{}'`, it'll
+    be replaced with the message.
+
+    :attr:`logged_pat` is a :kivy:class:`~kivy.properties.StringProperty` and
+    defaults to `'{}'`. See :class:`MoaObjectLogger` for an example.
+    '''
+
+    logger = ObjectProperty(logging.getLogger(__name__), allownone=True)
+    '''The logger object to which things are logged.
+
+    :attr:`logger` is a :kivy:class:`~kivy.properties.ObjectProperty` and
+    defaults to a Logger object with name `__name__`. If None, no logging will
+    occur.
+    '''
