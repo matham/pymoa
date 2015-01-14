a35f3a7c4659d02551400f6cc4ad51ac36965778
Matthew Einhorn <moiein2000@gmail.com>
2015-01-14 15:49:21 -0500

    Update config with a moa cifig file.

--- moa/__init__.py
+++ moa/__init__.py
@@ -1,3 +1,14 @@
 
 
 __version__ = '0.1-dev'
+
+from kivy import kivy_home_dir
+from os import environ
+from os.path import join
+
+
+#: moa configuration filename
+moa_config_fn = ''
+
+if not environ.get('MOA_DOC_INCLUDE'):
+    moa_config_fn = join(kivy_home_dir, 'moa_config.ini')
