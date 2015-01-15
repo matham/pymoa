7a8935e1bfa644d734ec843abc2c5f41c034ef75
Matthew Einhorn <moiein2000@gmail.com>
2015-01-14 22:42:52 -0500

    Emit moa version when starting.

--- moa/__init__.py
+++ moa/__init__.py
@@ -5,6 +5,7 @@ __version__ = '0.1-dev'
 from kivy import kivy_home_dir
 from os import environ
 from os.path import join
+from moa.logger import Logger
 
 
 #: moa configuration filename
@@ -12,3 +13,5 @@ moa_config_fn = ''
 
 if not environ.get('MOA_DOC_INCLUDE'):
     moa_config_fn = join(kivy_home_dir, 'moa_config.ini')
+
+Logger.info('Moa v%s' % (__version__))
