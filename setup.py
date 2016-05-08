7e4017433c125e7e6440c6c0dd0f67c19ff94e7d
Matthew Einhorn <moiein2000@gmail.com>
2016-05-08 18:45:37 -0400

    Update setup.

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 from setuptools import setup, find_packages
 import moa
 
+with open('README.rst') as fh:
+    long_description = fh.read()
 
 setup(
     name='Moa',
     version=moa.__version__,
-    packages=find_packages(),
-    package_data={
-        'moa': ['data/*.kv'],
-    },
-    install_requires=['kivy'],
     author='Matthew Einhorn',
     author_email='moiein2000@gmail.com',
     license='MIT',
-    description=(
-        'Kivy based experimental control.')
+    description='Kivy based scientific platform for running experiments.',
+    url='http://matham.github.io/moa/',
+    long_description=long_description,
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: System :: Hardware',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Operating System :: Microsoft :: Windows',
+                 'Intended Audience :: Developers'],
+    packages=find_packages(),
+    package_data={'moa': ['data/*.kv']},
+    install_requires=['kivy']
     )
