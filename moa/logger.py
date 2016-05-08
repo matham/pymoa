09f73fd483a63133669d6b81d28f6e2f4e7516a3
Matthew Einhorn <moiein2000@gmail.com>
2016-05-08 19:54:03 -0400

    Why is findCaller defined overwritten?

--- moa/logger.py
+++ moa/logger.py
@@ -80,29 +80,6 @@ class _MoaLoggerBase(logging.Logger):
             return KivyLogger.isEnabledFor(lvl)
         return False
 
-    def findCaller(self):
-        """
-        Find the stack frame of the caller so that we can note the source
-        file name, line number and function name.
-        """
-        if not self.name.startswith('moa'):
-            return super(_MoaLoggerBase, self).findCaller()
-        f = currentframe()
-        #On some versions of IronPython, currentframe() returns None if
-        #IronPython isn't run with -X:Frames.
-        if f is not None:
-            f = f.f_back
-        rv = "(unknown file)", 0, "(unknown function)"
-        while hasattr(f, "f_code"):
-            co = f.f_code
-            filename = os.path.normcase(co.co_filename)
-            if filename == _srcfile:
-                f = f.f_back
-                continue
-            rv = (co.co_filename, f.f_lineno, co.co_name)
-            break
-        return rv
-
 
 class KivyHandler(logging.Handler):
     '''The Moa handler class that forwards logs to kivy.
